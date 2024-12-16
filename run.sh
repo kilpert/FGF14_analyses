f4507d41a37f56598d688aa2a87d3caa67b791cc
Fabian Kilpert <fkilpert@gmail.com>
2024-12-16 10:49:37 +0100

    use profile "debug" to avoid crash

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete #--rerun-trigger mtime
+snakemake --use-conda -p --rerun-incomplete --profile debug #--rerun-trigger mtime
 
